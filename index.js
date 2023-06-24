30eaf1da32db68eafa249dbb85fb004e0bde6ef4
MissElena786 <bgulnaz009@gmail.com>
2023-06-24 19:58:10 +0530

    portfolio

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+
+$(".fa-square-xmark").css("display","none")
+$(".fa-bars").on('click', function(){
+    $(".nav").css("display", "block")
+    $(".fa-bars").css("display", "none")
+    $(".fa-square-xmark").css("display","block")
+
+})
+$(".fa-square-xmark").on('click', function(){
+    $(".nav").css("display", "none")
+    $(".fa-bars").css("display", "block")
+    $(".fa-square-xmark").css("display","none")
+
+})
+
+const body = $("body");
+
+
+// $(".day-night").click(function(){
+//     body.toggleClass("dark-body");
+//     $(".main").toggleClass("dark-main");
+//     $(".about").toggleClass("dark-about");
+//     $(".education").toggleClass("dark-education");
+//     $(".projects").toggleClass("dark-projects");
+//     $(".contact").toggleClass("dark-contact");
+//     $(".footer").toggleClass("dark-footer");
+
+
+   
+// });
+
+// 
+
+   
+
+ 
