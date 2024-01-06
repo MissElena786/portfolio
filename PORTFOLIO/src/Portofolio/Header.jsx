675b8210990a186a54f60ce1c887220a83b5e7ca
MissElena786 <bgulnaz009@gmail.com>
2024-01-06 16:47:02 +0530

    portfolio

--- PORTFOLIO/src/Portofolio/Header.jsx
+++ PORTFOLIO/src/Portofolio/Header.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from "../assets/logo.png"
+import "../mediaQuery.css"
 
 function Header() {
+    const [isMobile, setIsMobile] = useState(false);
+    const [hidden, setHidden] = useState(false);
+    const toggle = ()=>{
+      setHidden(!hidden)
+      console.log(hidden)
+    }
+  
+    useEffect(() => {
+      const mediaQuery = window.matchMedia('(max-width:840px)');
+  
+      const handleMediaQueryChange = (event) => {
+        setIsMobile(event.matches);
+      };
+  
+      handleMediaQueryChange(mediaQuery);
+        mediaQuery.addListener(handleMediaQueryChange);
+  
+      return () => {
+        mediaQuery.removeListener(handleMediaQueryChange);
+      };
+    }, []); 
   return (
-    <div>
-      <header>
-         <nav className='navbar'>
+    <div className={`navbar  ${isMobile ? 'mobile' : 'desktop'}`}> 
+         <nav className={`header my-component nav-items `}>
+          {isMobile ? 
+            <i className="fa-solid fa-bars" onClick={toggle}></i>
+            :
+            ""
+          }
           <div>  
             <img  className='logo' src={logo} alt="logo" />
           </div>
-             <ul className='nav-items'>
+             <ul  className={`my-component nav-items ${hidden ? "nav-hidden" : ""} `}>
                <li><Link to="/">Home</Link></li>
                <li><Link to="/about">About Us</Link></li>
                <li><Link to="/projects">Projects</Link></li>
@@ -18,7 +44,6 @@ function Header() {
                <li><Link to="/contact">Contact</Link></li>
              </ul>
          </nav>
-      </header>
       
     </div>
   )
--- PORTFOLIO/src/Portofolio/Projects.jsx
+++ PORTFOLIO/src/Portofolio/Projects.jsx
@@ -14,8 +14,11 @@ import num_gassing from "../assets/number-gassing.png"
 import movie from "../assets/movie-search (2).png"
 import landscap from "../assets/landscap.png"
 import gallery from "../assets/gallery.png"
+import reciep from "../assets/reciep.png"
+import { Link } from 'react-router-dom';
 
 function Projects() {
+    
   return (
     <div className='main'>
        <div className='project-container' >
@@ -26,7 +29,7 @@ function Projects() {
       <img src={ecommerce} alt="img" />
       <div className='inner-item'>
         <h4>Ecommerce</h4>
-        <p> ecommerce website which is build on MERN stack</p>
+        <p className='description'> ecommerce website which is build on MERN stack</p>
     </div>
     </div>
     <div className='item'>
@@ -34,40 +37,48 @@ function Projects() {
     <div className='inner-item'>
 
         <h4>Learning Management System</h4>
-        <p> learning management system which is build on MERN stack</p>
+        <p className='description'> learning management system which is build on MERN stack</p>
     </div>
     </div>
     <div className='item'>
+    <Link to="https://movie-search-app-pi-eight.vercel.app/">
+
     <img src={movie} alt="img" />
     <div className='inner-item'>
         <h4>Movie Search App</h4>
-        <p>movie search app you can search movie by the name</p>
+        <p className='description'>movie search app you can search movie by the name</p>
         </div> 
+        </Link>
     </div>
     <div className='item'>
+    <Link to="https://ecommerce-website-sand.vercel.app/">
     <img src={ecom_react} alt="img" />
     <div className='inner-item'>
+
         <h4>Ecommerce React frontend</h4>
-        <p> ecommerce which is built on react, react-redux. </p>
-        </div>    </div>
+        <p className='description'> ecommerce which is built on react, react-redux. </p>
+        </div> 
+        </Link>   </div>
     <div className='item'>
+    <Link to="https://gallery-project-eight.vercel.app/">
     <img src={gallery} alt="img" />
     <div className='inner-item'>
         <h4>Gallery App</h4>
-        <p> Gallery app the api. All photos fetching by the third Party api</p>
+        <p className='description'> Gallery app the api. All photos fetching by the third Party api</p>
         </div>
+    </Link>
     </div>
     <div className='item'>
     <img src={calcy} alt="img" />
     <div className='inner-item'>
         <h4>Calculator </h4>
-        <p>this is a calculator. you can perform arithmatic oprations</p>
+        <p className='description'>this is a calculator. you can perform arithmatic oprations</p>
         </div>    </div>
     <div className='item'>
     <img src={quiz} alt="img" />
     <div className='inner-item'>
         <h4>Quiz App</h4>
-        <p> quiz app, solve quiz and get score. dom manupulation project.</p>
+        <p className='description'> quiz app, solve quiz and get score. dom manupulation project.</p>
         </div>
         
     </div>
@@ -75,26 +86,37 @@ function Projects() {
     <img src={landscap} alt="img" />
     <div className='inner-item'>
         <h4>Landscap Website</h4>
-        <p> landscap website. built on HTML, Css , Javascript, and Node</p>
+        <p className='description'> landscap website. built on HTML, Css , Javascript, and Node</p>
         </div>    </div>
     <div className='item'>
     <img src={dice} alt="img" />
     <div className='inner-item'>
         <h4>Dice Game</h4>
-        <p>Dice game of the 2 players. dom manupulation project. </p>
+        <p className='description'>Dice game of the 2 players. dom manupulation project. </p>
         </div>    </div>
     <div className='item'>
     <img src={num_gassing} alt="img" />
     <div className='inner-item'>
         <h4>Number Gassing Game</h4>
-        <p> number gassing game this is also a dom manupulation project.</p>
+        <p className='description'> number gassing game this is also a dom manupulation project.</p>
         </div>    </div>
     <div className='item'>
+        <Link to="https://vegetables-website-eight.vercel.app/">
     <img src={ecom} alt="img" />
     <div className='inner-item'>
         <h4>Ecomerce website</h4>
-        <p>ecommerce website, built on HTML, Css, JavaScript , JQuery etc. </p>
+        <p className='description'>ecommerce website, built on HTML, Css, JavaScript , JQuery etc. </p>
         </div>
+      </Link>
+    </div>
+    <div className='item'>
+        <Link to="https://recipe-app.vercel.app/">
+    <img src={reciep} alt="img" />
+    <div className='inner-item'>
+        <h4>Reciep website</h4>
+        <p className='description'>Reciepe website, built on React by third party api. </p>
+        </div>
+      </Link>
     </div>
  
 </OwlCarousel>
